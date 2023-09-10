683b0161bbdecd4d0b2880054a4e3782f9931b8b
Roman <Roman@DESKTOP-BV18ITA>
2023-09-10 22:22:35 +1000

    Добавьте файлы проекта.

---
+++ ToDoList.Domain/Enum/Priority.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Domain.Enum
+{
+    public enum Priority
+    {
+        [Display(Name = "Несрочная")]
+        Easy = 1,
+        [Display(Name = "Умеренная")]
+        Medium = 2,
+        [Display(Name = "Критичная")]
+        Hard = 3
+    }
+}
---
+++ ToDoList.Domain/ViewModels/Task/CreateTaskViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoList.Domain.Enum;
+
+namespace ToDoList.Domain.ViewModels.Task
+{
+    public class CreateTaskViewModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Priority Priority { get; set; }
+    }
+}
